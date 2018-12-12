923664f8777cf119506e8ad78599bd78c45c7933
arkhvoid <43354994+arkhvoid@users.noreply.github.com>
2018-12-12 15:07:27 +0800

    Add files via upload

---
+++ test/src/test/java/FinalTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FinalTest {
+	
+	@Test
+	public void FinalTestTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+	
+}
