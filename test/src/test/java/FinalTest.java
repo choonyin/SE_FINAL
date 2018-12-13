f6beb9000e45831de0274b8857564bb211a58e4a
arkhvoid <43354994+arkhvoid@users.noreply.github.com>
2018-12-13 14:20:01 +0000

    Update FinalTest.java

--- test/src/test/java/FinalTest.java
+++ test/src/test/java/FinalTest.java
@@ -15,25 +15,4 @@ public class FinalTest {
 		assertFalse(Memory_Management.isNumeric("0.1"));
 	}
 	
-	@Test
-	public void FIFOShouldReturnExpectedNumber() {
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
-		assertEquals(9, Memory_Management.FIFO(bay, frame));
-	}
-	
-	@Test
-	public void LRUShouldReturnExpectedNumber() {
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
-		assertEquals(10, Memory_Management.LRU(bay, frame));
-	}
-	
-	@Test
-	public void OptimalShouldReturnExpectedNumber() {
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
-		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
-		assertEquals(7, Memory_Management.Optimal(bay, frame));
-	}
-	
 }
