52b7635d21dcb3b09894fe130bc1f98620f84f9c
choonyin <45838072+choonyin@users.noreply.github.com>
2018-12-14 18:03:16 +0800

    Update FinalTest.java

--- src/test/java/FinalTest.java
+++ src/test/java/FinalTest.java
@@ -7,10 +7,20 @@ import org.junit.Test;
 
 public class FinalTest {
 	
+	List<String> bay = new ArrayList<>();
+        int frame = 3;
+	
 	@Test
 	public void isNumericShouldWork(){
 		assertTrue(Memory_Management.isNumeric("1"));
 		assertFalse(Memory_Management.isNumeric("a"));
+	}
+	
+	@Test
+	public void FIFOShouldReturnExpectedNumber() {
+		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("1"); bay.add("2"); bay.add("5"); 
+		bay.add("1"); bay.add("2"); bay.add("3"); bay.add("4"); bay.add("5");
+		assertEquals(9, Memory_Management.FIFO(bay, frame));
 	}
 	
 }
