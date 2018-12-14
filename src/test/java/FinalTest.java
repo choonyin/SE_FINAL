90aad2d82f68af15df6470d902dcf4a67a06ee10
arkhvoid <43354994+arkhvoid@users.noreply.github.com>
2018-12-14 16:24:14 +0800

    Update FinalTest.java

--- src/test/java/FinalTest.java
+++ src/test/java/FinalTest.java
@@ -6,13 +6,6 @@ import java.util.List;
 import org.junit.Test;
 
 public class FinalTest {
-	List<String> bay = new ArrayList<>();
-	int frame = 3;
 	
-	@Test
-	public void isNumericShouldWork() {
-		assertTrue(Memory_Management.isNumeric("1"));
-		assertFalse(Memory_Management.isNumeric("0.1"));
-	}
 	
 }
